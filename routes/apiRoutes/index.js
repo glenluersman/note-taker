31dedcef96cb704819d7016d34aa3052237f80c2
glenluersman <glen.luersman@gmail.com>
2022-01-20 18:38:57 -0500

    modularized routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
-const { getNotes, saveNote, deleteNote} = require('../../lib/notes');
+const { createNewNote, } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     let results = notes;
     if (req) {
-        results = getNotes(req, results);
-    } else {
-        res.sendStatus(404);
+        results = createNewNote(req, results);
     }
+    res.json(results);
 });
 
 router.post('/notes', (req, res) => {
